fac37f93fdef20b71760f9a9102c9b5af22257cd
mpellino <marcopellino00@gmail.com>
2023-04-02 21:08:22 +0200

    Create Following and Post models.
    Following is a manyToMany Relationship

--- network/models.py
+++ network/models.py
@@ -4,3 +4,34 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Following(models.Model):
+    user = models.ManyToManyField(User, on_delete=models.CASCADE, related_name="following")
+    following = models.ManyToManyField(User, on_delete=models.CASCADE, related_name="followers")
+
+    def __str__(self):
+        return f"{self.user} follows {self.following}"
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, blank=True, related_name="liked_posts")
+
+
+    def __str__(self):
+        return f"{self.user} {self.timestamp.strftime('%b %d %Y, %I:%M %p')}"
+
+    # This method is used to serialize the data to JSON, but I would stick with the
+    # default serializer for now
+    # def serialize(self):
+    #     return {
+    #         "id": self.id,
+    #         "content": self.content,
+    #         "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+    #         "likes": self.likes.count(),
+    #         "user": self.user.username
+    #     }
+
